c00e04e6e97cf93a8f14e0d6c61c70096a3052a8
annikakoeni <80926929+annikakoeni@users.noreply.github.com>
2022-07-11 20:42:20 +0200

    Upload

---
+++ Übung/homeworkone.js
@@ -0,0 +1,67 @@
+//a
+
+number = Math.pow (5, 2);
+inputfeld = prompt("Gib Zahl an");
+console.log (inputfeld);
+function Berechnen (number,inputfeld) {
+number = Math.sqrt(number);
+var ergebnis = document.getElementById ("Return");
+elem.innerHTML = "Ergebnis:" + value;
+}
+
+
+var number = rechnen();
+
+function rechnen() {
+    var number = x;
+    number = Math.pow(5, 2);
+    let number = Math.sqrt(number);
+    return number;
+}
+
+
+var number = "";
+function berechnen (number){
+    number = Math.sqrt (25);
+    console.log (number);
+    return (number);
+
+
+  //a
+}
+var number1 = 5;
+function Rechnung (number){
+
+number = Math.pow (number, 2);
+number = Math.sqrt (number);
+return number;
+}
+console.log(Rechnung (number1));
+
+
+//b
+const freundeNamen = ["eugen", "marcel", "dominik", "sarah", "alex", "rapha", "felix", "allekrasnovs", "flo"];
+console.log (freundeNamen);
+//e
+
+var university = ["Hochschule Furtwangen University"];
+var i = "";
+string [1] split = university.split ("");
+for ( int i =0< split.length; i++){
+    console.writeLine(split [i]);
+}
+Console.ReadLine();
+
+
+var words = tbt.Split(new char["Hochschule Furtwangen University"]{});
+1 = words[0]
+2 = words[1]
+3 = words[2]
+
+//c
+var university = "Hochschule Furtwangen University";
+var words = university.split (' ');
+var word1 = words[0];
+var word2 = words[1];
+var word3 = words[2];
+console.log (word2);
